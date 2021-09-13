e1b518544f944101107f924cf698e3c7e945cd48
BUUZKA <72550805+BUUZKA@users.noreply.github.com>
2021-09-13 15:21:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('WITAJ SWIECIE!')
 });
+
+app.all('/wiki', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+const wiki = require('./wiki.js');
+// ...
+app.use('/wiki', wiki);
