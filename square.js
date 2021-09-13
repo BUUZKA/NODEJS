89e91e24e03fe69e86122e8d0bc1d3acd2dcc3a0
BUUZKA <72550805+BUUZKA@users.noreply.github.com>
2021-09-13 15:21:24 +0200

    Update square.js

--- square.js
+++ square.js
@@ -3,3 +3,8 @@ exports.perimeter = function(width) { return 4 * width; };
 
 const square = require('./square'); // Here we require() the name of the file without the (optional) .js file extension
 console.log('The area of a square with a width of 4 is ' + square.area(4));
+
+setTimeout(function() {
+    console.log('First');
+    }, 6000);
+ console.log('Second');
